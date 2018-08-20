13bba881c44c79d110bd5050372b1714b178ee81
Shaikh Akbar <akbarshaikh1234@gmail.com>
2018-08-20 22:28:46 +0530

    Initial commit

---
+++ index.php
@@ -0,0 +1,243 @@
+<?php 
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Page Title</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="./css/main.css" />
+    <script src="main.js"></script>
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+</head>
+<body>
+    
+        <i class="material-icons scrolltop">keyboard_arrow_up</i>
+    
+    <div class="container-fluid">
+        <div class="row shadow-light fixed-top align-items-center">
+            <div class="col-lg-1 col-xl-1 col-md-1 col-sm-1">
+                
+            </div>
+            <div class="col-lg-7 col-xl-7 col-md-7 col-sm-  ">
+                <img class="navbar-brand text-hide justify-content-center" src="./assets/codedeltatxt.png" width="100px"> 
+            </div>
+
+            <div class="col-lg-4 col-xl-4 col-md-12 col-sm-12">
+                <nav class="nav justify-content-center nav-padding-top-bottom menu-hover">
+                    <a class="nav-link" href="#" id="about-link">ABOUT</a>
+                    <a class="nav-link" href="#" id="project-link">PROJECTS</a>
+                    <a class="nav-link" href="#" id="blog-link">BLOG</a>
+                    <a class="nav-link" href="#" id="contact-link">CONTACT</a>                
+               </nav>
+            </div>
+        </div>
+
+        <!-- <div class="row" style="margin-top:-50px;z-index:-21;">
+            <div class="">
+                 <img class="" name="img1" src="./assets/programmer2.png" width="100%" height="750px">
+                 <div style="width:150px;height:150px;background-color:red;margin:0px auto;z-index:188;border-radius:50%;">
+                    
+                 </div>
+            </div>
+        </div> -->
+    </div>
+
+    <div class="container" id="container1">
+        <div class="row">
+            <div class="col-lg-12 col-xl-12 col-md-12 col-sm-12">
+                
+                <div class="test">
+                    <div class="jumbotron jumbotron-background">
+                        <a name="about">
+                            <h1>ABOUT</h1>      
+
+                            <div class="jumbotron">
+                                <p>Bootstrap is the most popular HTML, CSS, and JS framework for developing responsive, mobile-first projects on the web.</p>
+                                    <br>
+                                <div style="clear:both;">
+                                    <h1>Bootstrap Tutorial</h1>      
+                                    <p>Bootstrap is the most popular HTML, CSS, and JS framework for developing responsive, mobile-first projects on the web.</p>
+                                </div>
+                                    <br>
+                                <div style="clear:both;">
+                                    <h1>Bootstrap Tutorial</h1>      
+                                    <p>Bootstrap is the most popular HTML, CSS, and JS framework for developing responsive, mobile-first projects on the web.</p>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="container" id="container2">
+        <div class="row">
+            <div class="col-lg-12 col-xl-12 col-md-12 col-sm-12">
+                
+                <div class="test">
+                    <div class="jumbotron jumbotron-background">
+                        <a name="about">
+                            <h1>Projects</h1>      
+
+                            <div class="jumbotron">
+                                <p>Bootstrap is the most popular HTML, CSS, and JS framework for developing responsive, mobile-first projects on the web.</p>
+                                    <br>
+                                <div style="clear:both;">
+                                    <h1>Bootstrap Tutorial</h1>      
+                                    <p>Bootstrap is the most popular HTML, CSS, and JS framework for developing responsive, mobile-first projects on the web.</p>
+                                </div>
+                                    <br>
+                                <div style="clear:both;">
+                                    <h1>Bootstrap Tutorial</h1>      
+                                    <p>Bootstrap is the most popular HTML, CSS, and JS framework for developing responsive, mobile-first projects on the web.</p>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="container" id="container3">
+        <div class="row">
+            <div class="col-lg-12 col-xl-12 col-md-12 col-sm-12">
+                
+                <div class="test">
+                    <div class="jumbotron jumbotron-background">
+                        <a name="about">
+                            <h1>BLOGS</h1>      
+
+                            <div class="jumbotron">
+                                <p>Bootstrap is the most popular HTML, CSS, and JS framework for developing responsive, mobile-first projects on the web.</p>
+                                    <br>
+                                <div style="clear:both;">
+                                    <h1>Bootstrap Tutorial</h1>      
+                                    <p>Bootstrap is the most popular HTML, CSS, and JS framework for developing responsive, mobile-first projects on the web.</p>
+                                </div>
+                                    <br>
+                                <div style="clear:both;">
+                                    <h1>Bootstrap Tutorial</h1>      
+                                    <p>Bootstrap is the most popular HTML, CSS, and JS framework for developing responsive, mobile-first projects on the web.</p>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="container" id="container4">
+        <div class="row">
+            <div class="col-lg-12 col-xl-12 col-md-12 col-sm-12">
+                
+                <div class="test">
+                    <div class="jumbotron jumbotron-background">
+                        <a name="about">
+                            <h1>Contacts</h1>      
+
+                            <div class="jumbotron">
+                                <p>Bootstrap is the most popular HTML, CSS, and JS framework for developing responsive, mobile-first projects on the web.</p>
+                                    <br>
+                                <div style="clear:both;">
+                                    <h1>Bootstrap Tutorial</h1>      
+                                    <p>Bootstrap is the most popular HTML, CSS, and JS framework for developing responsive, mobile-first projects on the web.</p>
+                                </div>
+                                    <br>
+                                <div style="clear:both;">
+                                    <h1>Bootstrap Tutorial</h1>      
+                                    <p>Bootstrap is the most popular HTML, CSS, and JS framework for developing responsive, mobile-first projects on the web.</p>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+
+<script>
+    
+    $(document).ready(function(){
+        $("#about-link").on("click",function(){
+           var pos= $("#container1").offset().top;
+           var position=pos-83;
+
+           $("HTML,BODY").animate({scrollTop:position},1000);
+        });
+
+        $("#project-link").on("click",function(){
+           var pos= $("#container2").offset().top;
+           var position=pos-83;
+
+           $("HTML,BODY").animate({scrollTop:position},1000);
+        });
+
+        $("#blog-link").on("click",function(){
+           var pos= $("#container3").offset().top;
+           var position=pos-83;
+
+           $("HTML,BODY").animate({scrollTop:position},1000);
+        });
+
+        $("#contact-link").on("click",function(){
+           var pos= $("#container4").offset().top;
+           var position=pos-83;
+
+           $("HTML,BODY").animate({scrollTop:position},1000);
+        });
+
+        $(".scrolltop").on("click",function(){
+            $("HTML,BODY").animate({scrollTop:0},1500);
+        });
+
+       /* $("#container2, #container3, #container4").hide();
+
+      $("a").click(function(){
+          var indexClick=$("a").index(this);
+          console.log(indexClick);
+
+          if(indexClick == 0){
+            $("#container2, #container3, #container4").hide();
+            $("#container1").show();
+        }else if(indexClick == 1){
+            $("#container2").show
+            $("#container1, #container3, #container4").hide();
+            
+         }else if(indexClick == 2){
+            $("#container1, #container2, #container4").hide();
+            $("#container3").show();
+        }else if(indexClick == 3){
+            $("#container1, #container2, #container3").hide();
+            $("#container4").show();
+        }
+
+      });*/
+      
+    });
+</script>
+
+</body>
+</html>
